9d68a3fe060cb381d9498ecb117936d7773604e1
Ruben <rubenkamstra@gmail.com>
2022-02-13 17:08:00 +0100

    Add files via upload

---
+++ nagios_install_nrpe server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "*installeren nagios-nrpe-server*"
+
+sleep 3
+
+sudo apt install nagios-plugins nagios-nrpe-server
+
+echo "*nagios-nrpe-server geinstalleerd*"
+
+echo "*openen nrpe.cfg*"
+sleep 5
+
+sudo nano /etc/nagios/nrpe.cfg
+
+echo "*restarting nagios-nrpe-server*"
+sleep 3
+
+sudo systemctl restart nagios-nrpe-server
+
+echo "installatie voltooid"
+
